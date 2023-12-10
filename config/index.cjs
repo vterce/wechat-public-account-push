874cfe68f70934ad3fea8649c727128a47fe1432
vterce <112741503+vterce@users.noreply.github.com>
2023-12-10 17:50:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '过程',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oS6bC6lKUBHo0b8XMLegut5LJSlM',
     }
